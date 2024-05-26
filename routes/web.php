90b7a7483cc75e5d7748740fca93780755202390
bellaarsita <bella20gintingm50@gmail.com>
2024-05-26 10:16:55 +0700

    login register done

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\TermsPage;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LandingPageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,9 +20,15 @@ use App\Http\Controllers\LandingPageController;
 |
 */
 
-Route::get('/', [LandingPageController::class, 'index']);
+Route::get('/', [LandingPageController::class, 'index'])->name('LandingPage.LandingIndex');
 Route::get('/history', [HistoryController::class, 'index']);
 
 Route::get('/product', [ProductController::class, 'show']);
 
-Route::get('/products', [ProductsController::class, 'show']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/check', [LoginController::class, 'check'])->name('check');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register2', [RegisterController::class, 'store'])->name('register2');
+
+Route::get('/termsandconditions', [TermsPage::class,'show']);
