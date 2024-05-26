df7e185251f0d9b876d90c173fcf78f9d6f7d923
shamgare <stsidkenuu@gmail.com>
2024-05-26 19:43:40 +0700

    Added Cart Page Frontend

--- resources/views/Product/product.blade.php
+++ resources/views/Product/product.blade.php
@@ -4,7 +4,6 @@
 <div class="container mt-4">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/">Home</a></li>
             <li class="breadcrumb-item"><a href="/products">Products</a></li>
             <li class="breadcrumb-item active" aria-current="page">Actifed Plus Expectorant</li>
         </ol>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+<<<<<<< Updated upstream
+=======
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\LoginController;
+>>>>>>> Stashed changes
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\LandingPageController;
@@ -18,4 +23,18 @@ use App\Http\Controllers\LandingPageController;
 Route::get('/', [LandingPageController::class, 'index']);
 Route::get('/history', [HistoryController::class, 'index']);
 
+<<<<<<< Updated upstream
 Route::get('/product', [ProductController::class, 'show']);
+=======
+Route::get('/product', [ProductController::class, 'show']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/check', [LoginController::class, 'check'])->name('check');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register2', [RegisterController::class, 'store'])->name('register2');
+
+Route::get('/termsandconditions', [TermsPage::class,'show']);
+
+Route::get('/cart', [CartController::class, 'index']);
+>>>>>>> Stashed changes
