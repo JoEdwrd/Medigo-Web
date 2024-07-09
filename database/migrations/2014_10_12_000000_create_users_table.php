d7e869f5d0857fcfd52ffaca4463c522a9f206b7
bellaarsita <bella20gintingm50@gmail.com>
2024-07-09 21:54:55 +0700

    update register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->longText('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-        
+
             $table->rememberToken();
             $table->timestamps();
         });
