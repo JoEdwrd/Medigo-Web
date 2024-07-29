2b24c590e3edf89c102d092c269222f8ab34a0bb
gladysruslim <gladysruslim22@gmail.com>
2024-07-29 09:09:29 +0700

    Update TrackingOrderSeeder.php

--- database/seeders/TrackingOrderSeeder.php
+++ database/seeders/TrackingOrderSeeder.php
@@ -15,36 +15,48 @@ class TrackingOrderSeeder extends Seeder
     public function run(): void
     {
         TrackingOrder::create([
-            "transaction_id" => 847821,
-            'waiting_for_verification'=> "2024-07-09 05:37:01",
+            "transaction_id" => 1,
+            'waiting_for_verification' => "2024-07-09 05:37:01",
+            'waiting_for_payment' => "2024-07-09 06:54:07",
+            'in_progress' => "2024-07-09 07:22:03"
+        ]);
+        TrackingOrder::create([
+            "transaction_id" => 2,
+            'waiting_for_verification' => "2024-07-09 05:37:01",
+            'waiting_for_payment' => "2024-07-09 06:54:07",
+            'in_progress' => "2024-07-09 07:22:03",
+        ]);
+        TrackingOrder::create([
+            "transaction_id" => 3,
+            'waiting_for_verification' => "2024-07-09 05:37:01",
+        ]);
+        TrackingOrder::create([
+            "transaction_id" => 4,
+            'waiting_for_verification' => "2024-07-09 05:37:01",
+        ]);
+
+        TrackingOrder::create([
+            "transaction_id" => 5,
+            'waiting_for_verification' => "2024-07-09 05:37:01",
             'waiting_for_payment' => "2024-07-09 06:54:07",
             'in_progress' => "2024-07-09 07:22:03",
             'completed' => "2024-07-09 09:45:01"
         ]);
 
         TrackingOrder::create([
-            "transaction_id" => 1
-        ]);
-        TrackingOrder::create([
-            "transaction_id" => 2
-        ]);
-        TrackingOrder::create([
-            "transaction_id" => 3
-        ]);
-        TrackingOrder::create([
-            "transaction_id" => 4
+            "transaction_id" => 6,
+            'waiting_for_verification' => "2024-07-09 05:37:01",
+            'waiting_for_payment' => "2024-07-09 06:54:07",
+            'in_progress' => "2024-07-09 07:22:03",
+            'completed' => "2024-07-09 09:45:01"
         ]);
 
         TrackingOrder::create([
-            "transaction_id" => 5
-        ]);
-
-        TrackingOrder::create([
-            "transaction_id" => 6
-        ]);
-
-        TrackingOrder::create([
-            "transaction_id" => 7
+            "transaction_id" => 7,
+            'waiting_for_verification' => "2024-07-09 05:37:01",
+            'waiting_for_payment' => "2024-07-09 06:54:07",
+            'in_progress' => "2024-07-09 07:22:03",
+            'canceled' => "2024-07-09 09:45:01"
         ]);
     }
 }
